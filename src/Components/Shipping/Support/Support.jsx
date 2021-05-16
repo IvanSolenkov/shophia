3d760e07d86e92aa45239ebff05b044e63a76997
IvanSolenkov <ivan_solenkov1999@abv.bg>
2021-05-16 17:46:12 +0300

    Updates

---
+++ src/Components/Shipping/Support/Support.jsx
@@ -0,0 +1,15 @@
+import * as CgIcons from 'react-icons/cg';
+
+const Support = () => {
+    return (
+        <div className="support">
+            <CgIcons.CgSupport id="shipping-icon" />
+            <div>
+                <h4>24/7 Support</h4>
+                <span>Lifestyme Support</span>
+            </div>
+        </div>
+    );
+}
+
+export default Support;
