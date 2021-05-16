06ae651abae49cd64f667df6cef1a4a194027d59
IvanSolenkov <ivan_solenkov1999@abv.bg>
2021-05-16 17:15:55 +0300

    Updates

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+import * as FaIcons from 'react-icons/fa';
+import * as RiIcons from 'react-icons/ri';
 import Connection from "../FooterLists/Connection/Connection"
 import Information from "../FooterLists/Information/Information"
 import Service from "../FooterLists/Service/Service"
@@ -11,6 +13,33 @@ const Footer = () => {
                 <Information />
                 <Service />
                 <Connection />
+            </div>
+            <hr />
+            <div className="footer-bottom">
+                <div>
+                    <p>
+                        © 2015 Shopia Fashon Store Shopify.
+                    <br />
+                    All Rights Reserved. Ecommerce Software by Shopify.
+                    <br />
+                    Designed by Moccal.abs.com
+                    </p>
+                    <ul>
+                        <li>
+                            <FaIcons.FaCcVisa id="bank-icons" />
+                        </li>
+                        <li>
+                            <FaIcons.FaCcMastercard id="bank-icons" />
+                        </li>
+                        <li>
+                            <FaIcons.FaCcApplePay id="bank-icons" />
+                        </li>
+                        <li>
+                            <FaIcons.FaCcDiscover id="bank-icons" />
+                        </li>
+                    </ul>
+                </div>
+                <RiIcons.RiArrowUpCircleFill id="arrow-btn" />
             </div>
         </div>
     );
